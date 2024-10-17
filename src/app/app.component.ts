5b5aaeb2bd42117b353c554b5aaba76a8592aa4d
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2024-10-17 10:30:50 +0330

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { faker } from '@faker-js/faker'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'type';
+  randomText :string = faker.lorem.sentences(1);
 }
